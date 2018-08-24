a83eff08e9ff6d7bccc7ec9a3bd962ebce4ace61
jorgehuete <42672053+jorgehuete@users.noreply.github.com>
2018-08-24 14:28:07 +0200

    Update settings.php

--- moodle/theme/child/settings.php
+++ moodle/theme/child/settings.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @package    theme_parent
+ * @author     Jorge Huete & Carlos Perez
+ */
+
 include_once ('config.php');
 include_once $CFG->dirroot . '/theme/'.$THEME->name.'/classes/settings_child.php';
 require_once($CFG->libdir.'/adminlib.php');
